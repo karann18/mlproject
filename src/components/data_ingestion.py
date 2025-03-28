4d950a6a33cb05e3f47d0b7a92abe2ba39daa719
Karan <karanvishwakarma2718@gmail.com>
2025-03-28 17:29:40 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,8 +3,6 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
-
-
 from  sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
@@ -21,4 +19,29 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df+pd.read_csv('se')
+            df=pd.read_csv(r"C:\projects\mlproject\notebook\data\stud.csv")
+            logging.info('Read the data as datafarme')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info(" inegestion of the data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+            
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
